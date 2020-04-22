d36c083c3e6a47b97f1737e77f2c36ddb11feb94
Danny Peck <dannytpeck@gmail.com>
2020-04-22 14:16:41 -0700

    Update to new idea with row by row upload buttons

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
 import Airtable from 'airtable';
-const base = new Airtable({ apiKey: 'keyCxnlep0bgotSrX' }).base('appN1J6yscNwlzbzq');
+const base = new Airtable({ apiKey: 'keyCxnlep0bgotSrX' }).base('appHXXoVD1tn9QATh');
 
 import Header from './header';
 import Footer from './footer';
@@ -12,16 +12,20 @@ function clientsReducer(state, action) {
   return [...state, ...action];
 }
 
+function activitiesReducer(state, action) {
+  return [...state, ...action];
+}
+
 /* globals $ */
 function App() {
   const [selectedClient, setSelectedClient] = useState(null);
-  const [activities, setActivities] = useState([]);
-  const [selectedActivity, setSelectedActivity] = useState(null);
-  const [tiles, setTiles] = useState([]);
-
-  const [clients, dispatch] = React.useReducer(
+  const [clients, dispatchClients] = React.useReducer(
     clientsReducer,
     [] // initial clients
+  );
+  const [activities, dispatchActivities] = React.useReducer(
+    clientsReducer,
+    [] // initial activities
   );
 
   // When app first mounts, fetch clients
@@ -30,7 +34,7 @@ function App() {
     base('Clients').select({
       view: 'sorted'
     }).eachPage((records, fetchNextPage) => {
-      dispatch(records);
+      dispatchClients(records);
 
       fetchNextPage();
     }, (err) => {
@@ -42,50 +46,56 @@ function App() {
 
   }, []); // Pass empty array to only run once on mount
 
-  function massUpdater(activities) {
+  function massUpdater() {
 
-    const activitiesToUpdate = activities.filter(activity => {
+    const newHtml = `<p>Health &amp; Fitness focuses on key health metrics and managing the many aspects that contribute to your lifestyle, such as, nutrition, activity, sleep, and stress. This can help you achieve greater financial stability, improve your engagement and resilience at work, and help generate stronger connections to your community.</p><p style="margin-bottom: 10px;"><strong>Start by clicking one of our focused 6-week Paths below &mdash; and look for other resources with the red Health &amp; Fitness flag! Each features six interactive sessions along with weekly surveys (completion required to earn points).</strong></p><div class="aduro-app-callout" style="padding: 10px 10px; margin: 40 auto 20 auto;"><a href="https://amp.adurolife.com/referral/limeade-signup" target="_blank" rel="noopener"><img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/App_CTA_HF.png" alt="AMP callout" /></a></div><div class="one-on-one-callout" style="margin-bottom: 20px;"><a href="/api/redirect?url=https%3A//wellmetricssurveys.secure.force.com/Calendar/ProgramCalendarV2%3Fe=%5Be%5D%26formType=%26calendarName=Ignite+Your+Life%26participantCode=%5Bparticipantcode%5D" target="_blank" rel="noopener"><img class="one-on-one-image" style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/Coaching_CTA_HF.png" alt="One-on-One Coaching" /></a></div><div class="coaching-programs-container"><div class="coaching-program-callout stress_relief_toolkit" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DStress%20Relief%20Toolkit%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite%20Your%20Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-stress-toolkit.png" alt="Stress Relief Toolkit" /></a></div><div class="coaching-program-callout rethinking_stress" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DRethinking%20Stress%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite%20Your%20Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-rethinking-stress.png" alt="Rethinking Stress" /></a></div><div class="coaching-program-callout mission_nutr" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DMission%20Nutrition%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite%20Your%20Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-mission-nutrition.png" alt="Mission Nutrition" /></a></div><div class="coaching-program-callout mood_food" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DMood%20%252526%20Food%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite+Your+Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-mood-and-food.png" alt="Mood and Food" /></a></div><div class="coaching-program-callout lighten_up" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DLighten%20Up%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite%20Your%20Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-lighten-up.png" alt="Lighten Up" /></a></div><div class="coaching-program-callout breathe_easy" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DBreathe%20Easy%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite%20Your%20Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-breathe-easy.png" alt="Breathe Easy" /></a></div><div class="coaching-program-callout sleep_mode" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DSleep+mode%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite+Your+Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-sleep-mode.png" alt="Breathe Easy" /></a></div><div class="coaching-program-callout get_moving" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DGet%20Moving%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite%20Your%20Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-get-moving.png" alt="Breathe Easy" /></a></div><div class="coaching-program-callout fast_fitness" style="margin-bottom: 20px;"><a href="/api/Redirect?url=https%3A%2F%2Fwellmetricssurveys.secure.force.com%2FEvent%2FCoachingEventCheckin%3Fp%3D%5Be%5D%26cpName%3DFast%20Fitness%26participantCode%3D%5Bparticipantcode%5D%26eventType%3DIgnite%20Your%20Life" target="_blank" rel="noopener"> <img style="width: 100%;" src="https://cdn.adurolife.com/assets/hp/images/hp-tile-fast-fitness.png" alt="" /> </a></div></div>`;
 
-      // Skip CIEs and find tiles using old image server
-      if (activity.ChallengeId > 0 && activity.AboutChallenge.includes('mywellnessnumbers.com/ChallengeBank/')) {
+    const coachingClients = clients.filter(client => client.fields['Coaching'] === 'Yes');
+    console.log(coachingClients);
 
-        const today = moment();
-        const endDate = moment(activity.EndDate);
+    // Set counter based on coachingClients
+    $('#counter').html(`<p><span id="finishedUploads">0</span> / ${coachingClients.length}</p>`);
 
-        if (today < endDate) {
-          return true;
-        }
+    coachingClients.map(client => {
 
+      // 1 - Get current ID -2351 using limeade's modern API
+      if (client.fields['LimeadeAccessToken']) {
+
+        console.log('Getting CIE 2351 for ' + client.fields['Account Name']);
+
+        $.ajax({
+          url: 'https://api.limeade.com/api/admin/activity/-2351',
+          type: 'GET',
+          dataType: 'json',
+          headers: {
+            Authorization: 'Bearer ' + client.fields['LimeadeAccessToken']
+          },
+          contentType: 'application/json; charset=utf-8'
+        }).done((result) => {
+
+          // Advance the counter
+          let count = Number($('#finishedUploads').html());
+          $('#finishedUploads').html(count + 1);
+
+          const activity = result.Data;
+          activity.client = client;
+          dispatchActivities([...activities, activity]);
+
+        }).fail((xhr, textStatus, error) => {
+          console.error(`${client.fields['Account Name']} - GET ActivityLifecycle has failed`);
+        });
+
+      } else {
+        console.error(`${client.fields['Account Name']} has no LimeadeAccessToken`);
       }
 
-    });
-
-    console.log(activitiesToUpdate);
-
-    activitiesToUpdate.map(activity => {
-      const updatedAboutChallenge = activity.AboutChallenge.replace(/https:\/\/mywellnessnumbers.com\/ChallengeBank\//g, 'https://cdn.adurolife.com/dsjx/aduro/legacy/');
-      const data = {
-        AboutChallenge: updatedAboutChallenge
-      };
-
-      $.ajax({
-        url: 'https://api.limeade.com/api/admin/activity/' + activity.ChallengeId,
-        type: 'PUT',
-        data: JSON.stringify(data),
-        dataType: 'json',
-        headers: {
-          Authorization: 'Bearer ' + selectedClient.fields['LimeadeAccessToken']
-        },
-        contentType: 'application/json; charset=utf-8'
-      }).done((result) => {
-        console.log('Done', result);
-      });
+      // 2 - Update CIE 2351 using limeade's old transporter API
 
     });
 
   }
 
-  function getActivity() {
+  function getActivities() {
     if (selectedClient) {
       if (selectedClient.fields['LimeadeAccessToken']) {
 
@@ -108,7 +118,7 @@ function App() {
           // handles any updates needed for every activity in the platform
           // massUpdater(activities);
 
-          setActivities([...activities, activity]);
+          dispatchActivities([...activities, activity]);
 
         }).fail((xhr, textStatus, error) => {
           console.error(`${selectedClient.fields['Account Name']} - GET ActivityLifecycle has failed`);
@@ -136,52 +146,50 @@ function App() {
     });
   }
 
+  function performUpdate(activity) {
+    console.log(activity.client);
+
+    const employerName = activity.client.fields['Limeade e='];
+    $('#' + employerName.replace(/\s*/g, '')).addClass('bg-success');
+  }
+
   function renderActivities() {
 
-    // Filter out CIEs and past activities
-    const filteredActivities = activities.filter(activity => {
-      return activity.ChallengeId === -2351;
-    });
+    return activities.map((activity) => {
+      const employerName = activity.client.fields['Limeade e='];
 
-    return filteredActivities.map((activity) => {
       return (
-        <tr key={activity.employerName}>
-          <td>{activity.employerName}</td>
+        <tr id={employerName.replace(/\s*/g, '')} key={employerName}>
+          <td>{activity.client.fields['Account Name']}</td>
           <td>{activity.Name}</td>
-          <td>{activity.EventCode}</td>
           <td>{activity.ActivityReward.Value}</td>
           <td>{activity.DisplayPriority}</td>
+          <td>
+            <button type="button" className="btn btn-primary" onClick={() => performUpdate(activity)}>Upload</button>
+          </td>
         </tr>
       );
     });
-  }
 
-  console.log(activities);
+  }
 
   return (
     <div id="app">
       <Header />
 
       <div className="form-group">
-        <label htmlFor="employerName">EmployerName</label>
-        <select id="employerName" className="form-control custom-select" onChange={(e) => selectClient(e)}>
-          <option defaultValue>Select Employer</option>
-          {renderEmployerNames()}
-        </select>
+        <button type="button" className="btn btn-primary" onClick={massUpdater}>Download Activities</button>
+        <div id="counter"></div>
       </div>
-
-      <button type="button" className="btn btn-primary" onClick={getActivity}>I Want Everything</button>
-      <img id="spinner" src="images/spinner.svg" />
-      <p>(shows all challenges current and scheduled)</p>
 
       <table className="table table-hover table-striped" id="activities">
         <thead>
           <tr>
-            <th scope="col">EmployerName</th>
+            <th scope="col">Account Name</th>
             <th scope="col">Name</th>
-            <th scope="col">EventCode</th>
             <th scope="col">Points</th>
             <th scope="col">DisplayPriority</th>
+            <th scope="col">Upload</th>
           </tr>
         </thead>
         <tbody>
